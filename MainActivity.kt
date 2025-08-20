131bcfbc6135aaa6cfe19c08573db765711a05f8
rebekasultana2029-lab <rebekasultana2029@gmail.com>
2025-08-21 03:31:52 +0600

    MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,48 @@
+package com.skills.fps
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startBtn = findViewById<Button>(R.id.startBtn)
+        val stopBtn = findViewById<Button>(R.id.stopBtn)
+
+        startBtn.setOnClickListener {
+            if (canDrawOverlays()) {
+                startService(Intent(this, OverlayService::class.java))
+            } else {
+                requestOverlayPermission()
+            }
+        }
+
+        stopBtn.setOnClickListener {
+            stopService(Intent(this, OverlayService::class.java))
+        }
+    }
+
+    private fun canDrawOverlays(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Settings.canDrawOverlays(this)
+        } else true
+    }
+
+    private fun requestOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+        }
+    }
+}
